e51350ac2cacbbdc53f8b7ce535a707dfb40f8f8
HarishVS12 <harishv783@gmail.com>
2025-04-02 23:30:01 +0530

    FEATURE update string resource

--- composeApp/src/commonMain/kotlin/com/harish/photographyportfolio/main/presentation/components/gallery/GallerySection.kt
+++ composeApp/src/commonMain/kotlin/com/harish/photographyportfolio/main/presentation/components/gallery/GallerySection.kt
@@ -1,35 +1,22 @@
 package com.harish.photographyportfolio.main.presentation.components.gallery
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.snapping.SnapPosition
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
@@ -38,7 +25,7 @@ import com.harish.photographyportfolio.core.presentation.getPrimaryFontFamily
 import com.harish.photographyportfolio.core.util.getImageList
 import com.harish.photographyportfolio.core.util.toColor
 import harishphotographyportfolio.composeapp.generated.resources.Res
-import harishphotographyportfolio.composeapp.generated.resources.dummy_text
+import harishphotographyportfolio.composeapp.generated.resources.about_me_header
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -53,7 +40,7 @@ fun GallerySection(
     ) {
         Spacer(modifier = Modifier.height(24.dp))
         Text(
-            text = "Every photograph is a fragment of time, a moment captured before it fades. Through my lens, I invite you to see the world as I do—one frame at a time",
+            text = stringResource(Res.string.about_me_header),
             modifier = Modifier
                 .fillMaxWidth()
                 .align(Alignment.CenterHorizontally)
