f87820fc6b009b1d0d344ae927512121bd852708
HarishVS12 <harishv783@gmail.com>
2025-04-12 19:42:39 +0530

    FEATURE optimise imports

--- composeApp/src/commonMain/kotlin/com/harish/photographyportfolio/main/presentation/components/about/AboutSection.kt
+++ composeApp/src/commonMain/kotlin/com/harish/photographyportfolio/main/presentation/components/about/AboutSection.kt
@@ -1,8 +1,5 @@
 package com.harish.photographyportfolio.main.presentation.components.about
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.snapping.SnapPosition
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,9 +13,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
